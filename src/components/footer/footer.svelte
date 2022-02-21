771b59fd915912b9ad08705a31a29678363efe9d
woohm402 <woohm404@gmail.com>
2022-02-21 21:32:12 +0900

    [#3] implement footer component

---
+++ src/components/footer/footer.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Address from './address/address.svelte';
+  import Light from './assets/light.svg.svelte';
+  import Table from './table/table.svelte';
+</script>
+
+<footer>
+  <Table />
+  <Light />
+  <Address />
+</footer>
+
+<style>
+  footer {
+    width: 100%;
+    height: 738px;
+    background: #342d29;
+    position: relative;
+    text-align: left;
+    padding-inline-start: 63px;
+  }
+</style>
---
+++ src/components/footer/table/table.svelte
@@ -0,0 +1,22 @@
+<div class="table-top" />
+<div class="table-leg" />
+
+<style>
+  div.table-top {
+    position: absolute;
+    right: 0;
+    bottom: 248px;
+    width: 445px;
+    height: 23px;
+    background: #29151a;
+  }
+
+  div.table-leg {
+    position: absolute;
+    right: 374px;
+    bottom: 21px;
+    width: 23px;
+    height: 227px;
+    background: #29151a;
+  }
+</style>
