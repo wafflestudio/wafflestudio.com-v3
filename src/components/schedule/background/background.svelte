99d26c705448aa303ed061e7ab3a6fd0c4973f17
woohm402 <woohm404@gmail.com>
2022-03-07 23:41:53 +0900

    [#18] background 구현

---
+++ src/components/schedule/background/background.svelte
@@ -0,0 +1,38 @@
+<div class="tablecloth">
+  <div />
+  <img class="trident" src="images/trident.png" alt="background fork" />
+  <img class="sword" src="images/sword.png" alt="background knife" />
+</div>
+
+<style>
+  div.tablecloth {
+    position: absolute;
+    width: 1200px;
+    height: 500px;
+    box-sizing: border-box;
+    top: calc(50vh - 240px);
+    left: calc(50vw - 600px);
+    background-color: #986259;
+    padding-top: 130px;
+  }
+
+  div.tablecloth > div {
+    height: 240px;
+    width: 100%;
+    background-color: #f4987b;
+  }
+
+  img {
+    height: 600px;
+    position: absolute;
+    top: -30px;
+  }
+
+  img.trident {
+    left: 60px;
+  }
+
+  img.sword {
+    right: 60px;
+  }
+</style>
--- src/components/schedule/schedule.svelte
+++ src/components/schedule/schedule.svelte
@@ -1,9 +1,16 @@
-<section />
+<script>
+  import Background from './background/background.svelte';
+</script>
+
+<section>
+  <Background />
+</section>
 
 <style>
   section {
     background-color: #7e473e;
     width: 100vw;
     height: 100vh;
+    position: relative;
   }
 </style>
