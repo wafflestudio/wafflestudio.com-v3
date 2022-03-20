b6e15fabed28057a4317c98d768e924a4aa7342e
woohm402 <woohm404@gmail.com>
2022-03-20 12:24:40 +0900

    [#21] setup route

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,12 @@
-<script lang="ts">
-  import Banner from './components/banner/banner.svelte';
-  import About from './components/description/about/about.svelte';
-  import Member from './components/description/member.svelte';
-  import MainActivity from './components/activity/activity.svelte';
-  import Organization from './components/organization/organization.svelte';
-  import RecruitLink from './components/recruit-link/recruit-link.svelte';
-  import Activity from './components/description/activity.svelte';
-  import Schedule from './components/schedule/schedule.svelte';
-  import Footer from './components/footer/footer.svelte';
-  import Cover from './components/cover/cover.svelte';
-  import Operation from './components/operation/operation.svelte';
+<script>
+  import { Router, Route } from 'svelte-routing';
+  import Main from './pages/main.svelte';
+  import Project from './pages/project.svelte';
+
+  export let url = '';
 </script>
 
-<main>
-  <Cover />
-  <Banner />
-  <About />
-  <Member />
-  <Organization />
-  <MainActivity />
-  <RecruitLink />
-  <Activity />
-  <Schedule />
-  <Operation />
-  <Footer />
-</main>
-
-<style>
-</style>
+<Router {url}>
+  <Route path="/project" component={Project} />
+  <Route path="/" component={Main} />
+</Router>
---
+++ src/pages/main.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import Banner from '../components/banner/banner.svelte';
+  import About from '../components/description/about/about.svelte';
+  import Member from '../components/description/member.svelte';
+  import MainActivity from '../components/activity/activity.svelte';
+  import Organization from '../components/organization/organization.svelte';
+  import RecruitLink from '../components/recruit-link/recruit-link.svelte';
+  import Activity from '../components/description/activity.svelte';
+  import Schedule from '../components/schedule/schedule.svelte';
+  import Footer from '../components/footer/footer.svelte';
+  import Cover from '../components/cover/cover.svelte';
+  import Operation from '../components/operation/operation.svelte';
+</script>
+
+<main>
+  <Cover />
+  <Banner />
+  <About />
+  <Member />
+  <Organization />
+  <MainActivity />
+  <RecruitLink />
+  <Activity />
+  <Schedule />
+  <Operation />
+  <Footer />
+</main>
