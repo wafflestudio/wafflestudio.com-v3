7f171517e14926d18d32a425a12745caad7530d5
ars-ki-00 <dkwanm1@snu.ac.kr>
2022-03-21 22:41:58 +0900

    [#23] 푸터에 키보드 컴포넌트 추가

---
+++ src/components/footer/keyboard/keyboard.svelte
@@ -0,0 +1,11 @@
+<img class="keyboard" src="/assets/common/keyboard.svg" alt="" />
+
+<style>
+  .keyboard {
+    position: absolute;
+    width: 60%;
+    max-width: 1200px;
+    bottom: -15%;
+    right: -5%;
+  }
+</style>
