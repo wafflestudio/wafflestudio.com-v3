6a46ed880853046228cf6553605c534119d29dd4
ars-ki-00 <dkwanm1@snu.ac.kr>
2022-03-26 13:50:54 +0900

    [#59] 프로젝트 영역 배너 분리

--- src/components/projects/projects.svelte
+++ src/components/projects/projects.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import Banner from './banner.svelte';
+
   let current: 'snuboard' | 'snutt' | 'shiksha' = 'snuboard';
 </script>
 
@@ -62,11 +64,8 @@
       </li>
     </ul>
   </div>
-  <div class="banner">
-    <span>와플스튜디오의 서비스를 자세히 알아보세요!</span>
-    <a href="/">서비스 알아보기 ></a>
-  </div>
 </section>
+<Banner />
 
 <style>
   #projects {
@@ -217,50 +216,5 @@
     letter-spacing: -0.05em;
     text-align: center;
     color: #fff1cf;
-  }
-
-  #projects div.banner {
-    position: absolute;
-    display: flex;
-    align-items: center;
-    bottom: 0;
-    width: 100%;
-    height: 150px;
-    text-align: center;
-    vertical-align: middle;
-    background-color: #d57c6c;
-
-    justify-content: center;
-  }
-
-  #projects div.banner span {
-    font-size: 30px;
-    font-style: normal;
-    font-weight: 500;
-    line-height: 18px;
-    letter-spacing: -0.05em;
-    text-align: left;
-
-    margin-right: 100px;
-  }
-
-  #projects div.banner a {
-    display: inline-block;
-    width: 300px;
-    height: 65px;
-    margin-left: 50px;
-
-    font-size: 30px;
-    font-style: normal;
-    font-weight: 700;
-    line-height: 25px;
-    letter-spacing: -0.05em;
-    text-align: center;
-    vertical-align: middle;
-    color: #fff1cf;
-    border: 4px solid #fff1cf;
-    box-sizing: border-box;
-    border-radius: 15px;
-    line-height: 62px;
   }
 </style>
